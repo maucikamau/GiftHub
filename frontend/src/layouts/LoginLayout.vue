21f8b182ad695b9f8b462b2d6fc01ad2b4d87ea6
Domagoj Gavranić <domagoj.gavranic@fer.hr>
2025-10-30 16:41:21 +0100

    feat(frontend): added tailwindcss and basic login layout

---
+++ frontend/src/layouts/LoginLayout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="min-h-screen flex">
+    <div class="flex-1">
+      <slot name="preview" />
+    </div>
+    <div class="flex-1">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,20 @@
+<script setup lang="ts">
+import LoginLayout from "@/layouts/LoginLayout.vue";
+</script>
+
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <LoginLayout>
+    <div class="h-full grid place-items-center">
+      <div class="w-full max-w-[600px] mx-auto px-4 py-2">
+        <h1 class="font-bold text-2xl">You did it!</h1>
+        <p>
+          Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read
+          the
+          documentation
+        </p>
+      </div>
+    </div>
+  </LoginLayout>
 </template>
 
 <style scoped></style>
