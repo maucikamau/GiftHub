39e09001a47eea17758ae49dc352510549016e6f
Daniel <dz56235@fer.hr>
2026-01-21 16:35:23 +0100

    chore: popravljen v2 (valjda) reviews list

--- backend/backend/users/urls.py
+++ backend/backend/users/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 
 from .views import CreateUserView, UserList, user_delete_view, UserMeView, \
     user_basic_info_update_view, UserAdminView, UserLogoutView, RegisterAssociationView, user_update_role_view, \
-    CitiesView, UserUpdateView
+    CitiesView, UserUpdateView, UserUsernamesView
 from .views import user_redirect_view
 
 app_name = "users"
 urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("<int:pk>/", UserAdminView.as_view(), name="detail"),
+    path("username/<int:pk>/", UserUsernamesView.as_view(), name="username"),
     path("list/", UserList.as_view(), name="list"),
     path("register/", CreateUserView.as_view(), name="register"),
     path("register/role/", view=user_update_role_view, name="role"),
